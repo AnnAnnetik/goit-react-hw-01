9f1121d7564ec912154438af6c914cf5bdd27d07
AnnAnnetik <143535720+AnnAnnetik@users.noreply.github.com>
2024-03-07 10:21:52 +0200

    4

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,1 +1,27 @@
-const TransactionHistory = ({ items }) => {};
+const TransactionHistory = ({ items }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          {items.map(item => {
+            return (
+              <>
+                <td key={item.id}>{item.type}</td>
+                <td>{item.amount}</td>
+                <td>{item.currency}</td>
+              </>
+            );
+          })}
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+export default TransactionHistory;
