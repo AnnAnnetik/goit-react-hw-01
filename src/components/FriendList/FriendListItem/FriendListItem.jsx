5737e8971fd6b0fa2a637c5f414160b7b6a73b71
AnnAnnetik <143535720+AnnAnnetik@users.noreply.github.com>
2024-03-08 19:28:47 +0200

    hw

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,4 +1,4 @@
-import clsx from "clsx";
+
 import css from"./FriendListItem.module.css"
 const FriendListItem = ({ friend }) => {
     const friendIsonline = friend.isOnline;
@@ -7,10 +7,11 @@ const FriendListItem = ({ friend }) => {
           <li className={css.friendItem}>
       <img src={friend.avatar} alt="Avatar" width="48" />
               <p >{friend.name}</p>
-               <p className={clsx(css.friendListItem, { [css.full]: friendIsonline, })}>{friend.status}
-              {friendIsonline ? <span>Online</span> : <span>Offline</span>}</p>
+              {friendIsonline?(<p className={css.online}>Online</p>):(<p className={css.offline}>Offline</p>)}
           </li>
     </>
   );
 };
 export default FriendListItem
+{/* <p className={clsx(css.friendListItem, { [css.full]: friendIsonline, })}>{friend.status}
+              {friendIsonline ? <span>Online</span> : <span>Offline</span>}</p> */}
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,25 +1,26 @@
+import css from"./TransactionHistory.module.css"
 const TransactionHistory = ({ items }) => {
   return (
-    <table>
-      <thead>
+    <table className={css.conteyner}>
+      <thead className={css.hero}>
         <tr>
           <th>Type</th>
           <th>Amount</th>
           <th>Currency</th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
+      <tbody className={css.body}>
+       
           {items.map(item => {
             return (
-              <>
-                <td key={item.id}>{item.type}</td>
+             <tr key={item.id}>
+                <td >{item.type}</td>
                 <td>{item.amount}</td>
                 <td>{item.currency}</td>
-              </>
+              </tr>
             );
           })}
-        </tr>
+       
       </tbody>
     </table>
   );
