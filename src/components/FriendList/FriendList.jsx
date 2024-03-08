894948732cd17ce0f49fea7da226f0454e6df630
AnnAnnetik <143535720+AnnAnnetik@users.noreply.github.com>
2024-03-08 13:58:35 +0200

    fr

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,17 +1,17 @@
-// import FriendListItem from "./FriendListItem/FriendListItem";
-// import css from "./FriendList.module.css"
-// const FriendList = ({ friends }) => {
-//   return (
-//     <ul className={CSS.FriendList}>
-//       {Array.isArray(friends)&& friends.map((friend) => {
-//         return (
-//                      <FriendListItem key={friend.id} friend={friend}
+import FriendListItem from "./FriendListItem/FriendListItem";
+import css from "./FriendList.module.css"
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {Array.isArray(friends)&& friends.map((friend) => {
+        return (
+                     <FriendListItem key={friend.id} friend={friend}
               
-//             />
+            />
           
-//         );
-//       })}
-//     </ul>
-//   );
-// };
-// export default FriendList;
+        );
+      })}
+    </ul>
+  );
+};
+export default FriendList;
--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,14 +1,16 @@
-// import clsx from "clsx";
-// const FriendListItem = ({ friend }) => {
-//     const friendIsonline = friend.isOnline;
-//   return (
-//       <>
-//           <li className="">
-//       <img src={friend.avatar} alt="Avatar" width="48" />
-//       <p className={clsx(css.friendListItem,{[css.full]:friendIsonline,})}>{friend.name}</p>
-//               {friendIsonline ? <p>Online</p> : <p>Offline</p>}
-//           </li>
-//     </>
-//   );
-// };
-// export default FriendListItem
+import clsx from "clsx";
+import css from"./FriendListItem.module.css"
+const FriendListItem = ({ friend }) => {
+    const friendIsonline = friend.isOnline;
+  return (
+      <>
+          <li className={css.friendItem}>
+      <img src={friend.avatar} alt="Avatar" width="48" />
+              <p >{friend.name}</p>
+               <p className={clsx(css.friendListItem, { [css.full]: friendIsonline, })}>{friend.status}
+              {friendIsonline ? <span>Online</span> : <span>Offline</span>}</p>
+          </li>
+    </>
+  );
+};
+export default FriendListItem
